782a0027fb2b57b0c42371204ec3f7f42e2b22af
AGENT1736 <khaliddiab2003@gmail.com>
2024-05-20 18:26:59 +0300

    added ticket checking service added ticket sales service added a vending machine service added a train service

--- simulation.py
+++ simulation.py
@@ -1,1 +1,8 @@
 import simpy as sim
+
+def ticketService():
+    isPurchased = False
+    if(isPurchased == True):
+        print("Pass to the metro train")
+    else:
+        print("Access deined you need a ticket to enter!")
